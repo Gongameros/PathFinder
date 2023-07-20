99fcb1586189441d71bb7f14410938a4225e17e2
Markian Goinets <markiangonmark@gmail.com>
2023-07-20 21:34:25 +0300

    Made optimizations and added code comments.

--- fileFinder.cpp
+++ fileFinder.cpp
@@ -1,3 +1,14 @@
+/* This program searches for a specified file in a given directory and its subdirectories using multiple threads.
+// The search process is parallelized to improve efficiency, with a limit of MAX_THREADS concurrent search threads.
+// It employs atomics and mutexes to ensure thread safety and synchronized access to shared resources.
+// The findPathToFile function recursively explores the directory tree, spawning threads to search in subdirectories.
+// If the file is found, the program prints its path to the console and terminates all threads.
+// The program handles filesystem errors gracefully and prints a message if the file is not found or if the provided directory is invalid.
+// The initial function initiates the search process by creating a new thread for the findPathToFile function.
+// The program waits until the search is completed before exiting.
+*/
+
+
 #include "fileFinder.h"
 
 const uint8_t MAX_THREADS = 8;
@@ -16,50 +27,39 @@ void findPathToFile(const std::filesystem::path& directory, const std::string& f
             if (stopSearch)
                 return;
 
-            try 
+            if (dir_entry.is_directory())
             {
-                if (dir_entry.is_directory())
+                std::unique_lock<std::mutex> locker(mtxThread);
+                if (workingThreads < MAX_THREADS)
                 {
-                    std::unique_lock<std::mutex> locker(mtxThread);
-                    if (workingThreads < MAX_THREADS)
-                    {
-                        ++workingThreads;
-                        locker.unlock();
-                        std::thread searchThread(findPathToFile, dir_entry.path(), filename);
-                        searchThread.detach();
-                    }
-
-                    else
-                    {
-                        ++countAddThreads;
-                        locker.unlock();
-                        findPathToFile(dir_entry.path(), filename);
-                    }
+                    ++workingThreads;
+                    locker.unlock();
+                    std::thread searchThread(findPathToFile, dir_entry.path(), filename);
+                    searchThread.detach();
                 }
 
-                if (dir_entry.is_regular_file() && dir_entry.path().filename() == filename)
+                else
                 {
-                    std::unique_lock<std::mutex> ul(mtxPrint);
-                    std::cout << "Path for current file is: " << dir_entry.path() << std::endl;
-                    stopSearch = true;
-                    --workingThreads;
-                    ul.unlock();
-                    return;
+                    ++countAddThreads;
+                    locker.unlock();
+                    findPathToFile(dir_entry.path(), filename);
                 }
             }
-            catch (std::filesystem::filesystem_error& a)
-            {
-                continue;
-            }
 
-            catch (std::exception& ex)
+            if (dir_entry.is_regular_file() && dir_entry.path().filename() == filename)
             {
-                continue;
+                std::unique_lock<std::mutex> ul(mtxPrint);
+                std::cout << "Path for current file is: " << dir_entry.path() << std::endl;
+                stopSearch = true;
+                --workingThreads;
+                ul.unlock();
+                return;
             }
 
         }
     }
-    catch (std::exception& ex){}
+
+    catch (std::filesystem::filesystem_error& a){}
 
     if(workingThreads == MAX_THREADS && countAddThreads > 0)
         --countAddThreads;
